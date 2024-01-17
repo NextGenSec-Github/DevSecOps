83b07ca6230fa172e5e7fa7b3dfe7a4f6ab8f081
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-01-17 17:29:21 +0500

    Update kubectl-cheatsheet.sh

--- Kubernetes/Kubernetes Commands/kubectl-cheatsheet.sh
+++ Kubernetes/Kubernetes Commands/kubectl-cheatsheet.sh
@@ -1,1 +1,22 @@
+#KUBECTL COMMANDS
 
+#KUBECTL EXPLAIN:
+
+#The explain command in kubectl can tell us about the deployment object:
+kubectl explain deployment
+
+#You can also see all of the explanation fields using the --recursive option:
+kubectl explain deployment --recursive
+
+#You can use the explain command to help you understand the structure of a deployment object and understand what the individual fields do:
+kubectl explain deployment.metadata.name
+
+
+# Pod Commands
+echo "Pod Commands:"
+echo "--------------"
+echo "Create a pod: kubectl create pod <pod-name> --image=<image>"
+echo "Get pods: kubectl get pods"
+echo "Describe pod: kubectl describe pod <pod-name>"
+echo "Delete pod: kubectl delete pod <pod-name>"
+echo ""
