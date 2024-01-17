d5eea80f26cdba0c07d73380e0a3cd05f22a7863
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-01-17 17:08:47 +0500

    Update GKE-Command-Cheatsheet.sh

--- Google Cloud/Google Kubernetes Engine (GKE)/Hands-On/GKE-Command-Cheatsheet.sh
+++ Google Cloud/Google Kubernetes Engine (GKE)/Hands-On/GKE-Command-Cheatsheet.sh
@@ -3,3 +3,6 @@ gcloud container clusters create myclustername \
   --machine-type e2-small \
   --num-nodes 3 \
   --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
+
+# The explain command in kubectl can tell us about the deployment object:
+kubectl explain deployment
