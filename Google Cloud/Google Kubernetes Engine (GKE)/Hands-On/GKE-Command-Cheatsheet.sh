0fe24575c0615bac637dd948d8f84a586bf73d4c
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-01-17 17:07:45 +0500

    Update GKE-Command-Cheatsheet.sh

--- Google Cloud/Google Kubernetes Engine (GKE)/Hands-On/GKE-Command-Cheatsheet.sh
+++ Google Cloud/Google Kubernetes Engine (GKE)/Hands-On/GKE-Command-Cheatsheet.sh
@@ -1,1 +1,6 @@
+# Create a cluster with 3 nodes
 
+gcloud container clusters create myclustername \
+  --machine-type e2-small \
+  --num-nodes 3 \
+  --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
